5c323fbf5d67fedcbd3b3fca7cae18f2e615c921
Sergey Starodubcev <starodubcev_2018@list.ru>
2025-10-20 01:11:01 +0200

    Final iteration 1)Added exception class for input errors for controllers 2)Added Api Swagger for documentation controllers

--- src/main/java/com/example/springexample/cloudeservice/config/SecurityConfig.java
+++ src/main/java/com/example/springexample/cloudeservice/config/SecurityConfig.java
@@ -77,16 +77,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("*"));
-//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(List.of("*"));
-//        configuration.setAllowCredentials(true);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-
 }
